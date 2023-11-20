76f721e0823a875bd0b93bb82c3a8c6ea462d9b3
Curstantine <curstantine@gmail.com>
2023-11-20 21:38:52 +0530

    chore: dprint formatting, vite configuration and eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import solid from "solid-start/vite";
+import unocss from "unocss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [solid()],
+	plugins: [unocss(), solid()],
 });
