cdd593445df0e09922d8c024b08d0a21cd146da2
Curstantine <curstantine@gmail.com>
2024-01-27 11:09:28 +0530

    feat: add a projects list

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,7 @@
 import About from "~/components/About";
 import Attributions from "~/components/Attributions";
 import InfoContainer from "~/components/InfoContainer";
+import Projects from "~/components/Projects";
 import Skills from "~/components/Skills";
 import Tools from "~/components/Tools";
 
@@ -10,6 +11,7 @@ export default function Home() {
 			<InfoContainer />
 			<div class="flex flex-1 flex-col gap-2 px-6">
 				<About />
+				<Projects />
 				<Skills />
 				<Tools />
 				<Attributions />
