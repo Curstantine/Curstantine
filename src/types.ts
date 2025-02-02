e136cd8d16bff80255f5c388d3ebfc33f23221d1
Curstantine <curstantine@proton.me>
2025-02-02 22:49:21 +0530

    feat: include collaborated companies in the work

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,12 @@
+export interface Company {
+	name: string;
+	link: string;
+}
+
 export interface Project {
 	name: string;
 	description?: string;
+	asPartOf?: Company;
 	points: string[];
 	link: string;
 }
