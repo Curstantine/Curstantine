8962ab59bfc6cfc396adbed8ee254d5f4e457484
Curstantine <curstantine@proton.me>
2025-06-02 14:59:00 +0530

    feat: update tailwind config with colors and other configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,5 @@
 import js from "@eslint/js";
-import unocss from "@unocss/eslint-config/flat";
+
 import astro from "eslint-plugin-astro";
 import solid from "eslint-plugin-solid/configs/typescript";
 import ts from "typescript-eslint";
@@ -10,7 +10,6 @@ export default ts.config(
 	{ ignores: [".vercel", ".astro", "dist"] },
 	js.configs.recommended,
 	ts.configs.recommended,
-	unocss,
 	astro.configs.recommended,
 	{
 		files: ["**/*.{ts,tsx}"],
