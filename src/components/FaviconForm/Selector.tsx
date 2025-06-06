2a268419cca7fdfb1722d133114b5dffe8dbecff
Curstantine <curstantine@proton.me>
2025-06-06 17:27:26 +0530

    feat: icon canvas with background color changes

--- src/components/FaviconForm/Selector.tsx
+++ src/components/FaviconForm/Selector.tsx
@@ -1,33 +1,84 @@
 import type { JSX } from "solid-js";
-import { createSignal, Show } from "solid-js";
+import { createEffect, createSignal, onCleanup, Show } from "solid-js";
+
+import { contain } from "~/utils/canvas";
+import { type HSLA, toHSLAString } from "~/utils/colors";
 
 import { useFaviconForm } from "~/components/FaviconForm/context";
 
 export default function FaviconSelector() {
 	const { state, setState } = useFaviconForm();
 
-	return (
-		<Show
-			when={state.file}
-			fallback={<Input />}
-		>
-			{(file) => (
-				<div class="group relative size-44 inline-flex flex-col items-center justify-center border border-text-3 border-dashed">
-					<img
-						class="size-full object-cover"
-						src={URL.createObjectURL(file())}
-						alt="Preview"
-					/>
+	let canvasRef!: HTMLCanvasElement;
+	let offscreen!: HTMLCanvasElement;
 
-					<button
-						type="button"
-						class="absolute right-1 top-1 grid size-6 place-items-center bg-background/75 opacity-0 transition-opacity group-hover:opacity-100"
-						onClick={() => setState("file", null)}
-					>
-						<div class="iconify material-symbols--close-small size-5" />
-					</button>
-				</div>
-			)}
+	const init = (e: HTMLCanvasElement) => {
+		canvasRef = e;
+
+		if (!offscreen) {
+			offscreen = document.createElement("canvas");
+			offscreen.width = canvasRef.width;
+			offscreen.height = canvasRef.height;
+		}
+
+		if (!state.file) return;
+		const ctx = offscreen.getContext("2d")!;
+
+		const img = new Image();
+		img.src = URL.createObjectURL(state.file);
+		img.onload = () => {
+			const {
+				offsetX,
+				offsetY,
+				width,
+				height,
+			} = contain(canvasRef.width, canvasRef.height, img.width, img.height);
+			ctx.drawImage(img, offsetX, offsetY, width, height);
+			render(state.bgColor);
+		};
+	};
+
+	const closeFile = () => {
+		const ctx = offscreen.getContext("2d")!;
+		ctx.clearRect(0, 0, offscreen.width, offscreen.height);
+		setState("file", null);
+	};
+
+	const render = (bgColor: HSLA) => {
+		if (!canvasRef) return;
+		const ctx = canvasRef.getContext("2d")!;
+
+		ctx.fillStyle = toHSLAString(bgColor);
+		ctx.fillRect(0, 0, canvasRef.width, canvasRef.height);
+		ctx.drawImage(offscreen, 0, 0);
+	};
+
+	createEffect(() => {
+		// Note: state is a proxied store. The tracking scope only knows to fire when
+		// a nested property like bgColor.h is changed. So we need to defined all of the
+		// acceptable changes like this
+		render({ h: state.bgColor.h, s: state.bgColor.s, l: state.bgColor.l, a: state.bgColor.a });
+
+		onCleanup(() => {
+			if (!canvasRef) return;
+			const ctx = canvasRef.getContext("2d")!;
+			ctx.clearRect(0, 0, canvasRef.width, canvasRef.height);
+		});
+	});
+
+	return (
+		<Show when={state.file} fallback={<Input />}>
+			<div class="group relative size-44 inline-flex flex-col items-center justify-center border border-text-3 border-dashed">
+				<canvas ref={(e) => init(e)} width={300} height={300} class="size-full" />
+
+				<button
+					type="button"
+					class="absolute right-1 top-1 grid size-6 place-items-center bg-background/75 opacity-0 transition-opacity group-hover:opacity-100"
+					onClick={closeFile}
+				>
+					<div class="iconify material-symbols--close-small size-5" />
+				</button>
+			</div>
 		</Show>
 	);
 }
